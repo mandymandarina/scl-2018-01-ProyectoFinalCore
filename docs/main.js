5503f55e3706ec1a3be81948c7764250b59e9171
Karyn Herrera <karynherrera@gmail.com>
2018-07-31 09:39:56 -0400

    se añade boton para vincular las secciones

--- docs/main.js
+++ docs/main.js
@@ -1,29 +1,29 @@
 window.listaEmpresas = [];
 window.onload = (() => {
 
-const lista = document.getElementById("listaEmpresas");
+  const lista = document.getElementById("listaEmpresas");
   fetch('../data/empresas.json')
-		.then(response => response.json()) 
-		.then(data => {
+    .then(response => response.json())
+    .then(data => {
       data.forEach(element => {
         console.log(element.name);
       });
-			data.forEach(element => {
+      data.forEach(element => {
         let optionEmpresa = document.createElement('option');
         optionEmpresa.text = element.name;
         lista.add(optionEmpresa);
-      }); 
+      });
       console.log("holi");
-		})
- 
-});// fin de window onload
+    })
+  });// fin de window onload
+
 
 const seccionPrincipal = document.getElementById("sectionPrincipal");
 const seccionVisitas = document.getElementById("sectionVisitas");
 const seccionEncomiedas = document.getElementById("sectionEncomiendas");
 const seccionRvaEspacios = document.getElementById("sectionEspacios");
 
-const btnAtras = document.getElementById("btnAtras");
+//const btnAtras = document.getElementsByClassName("btnAtras");
 const btnVisitas = document.getElementById("btnVisitas");
 const btnEncomiedas = document.getElementById("btnEncomiendas");
 const btnRvaEspacios = document.getElementById("btnReservaEspacios");
@@ -32,27 +32,44 @@ btnVisitas.addEventListener('click', () => {
   seccionPrincipal.style.display = 'none';
   seccionVisitas.style.display = 'block';
   seccionEncomiedas.style.display = 'none';
-  seccionRvaEspacios.style.display = 'none'; 
+  seccionRvaEspacios.style.display = 'none';
 });
 
 btnEncomiedas.addEventListener('click', () => {
   seccionPrincipal.style.display = 'none';
   seccionVisitas.style.display = 'none';
   seccionEncomiedas.style.display = 'block';
-  seccionRvaEspacios.style.display = 'none'; 
+  seccionRvaEspacios.style.display = 'none';
 });
 
 btnRvaEspacios.addEventListener('click', () => {
   seccionPrincipal.style.display = 'none';
   seccionVisitas.style.display = 'none';
   seccionEncomiedas.style.display = 'none';
-  seccionRvaEspacios.style.display = 'block'; 
+  seccionRvaEspacios.style.display = 'block';
 });
 
-btnAtras.addEventListener('click', () => {
-  
-  seccionPrincipal.style.display = 'none';
-  seccionVisitas.style.display = 'none';
-  seccionEncomiedas.style.display = 'none';
-  seccionRvaEspacios.style.display = 'block'; 
+const atras=(()=>{
+  if (seccionRvaEspacios.style.display = 'block') {
+    seccionPrincipal.style.display = 'none';
+    seccionVisitas.style.display = 'none';
+    seccionEncomiedas.style.display = 'none';
+  }
+  if (seccionEncomiedas.style.display = 'block') {
+    seccionPrincipal.style.display = 'none';
+    seccionVisitas.style.display = 'none';
+    seccionRvaEspacios.style.display = 'none';
+  }
+  if (seccionVisitas.style.display = 'block') {
+    seccionPrincipal.style.display = 'none';
+    seccionRvaEspacios.style.display = 'none';
+    seccionEncomiedas.style.display = 'none';
+  }
+  if (seccionPrincipal.style.display = 'block') {
+    seccionEncomiedas.style.display = 'none';
+    seccionVisitas.style.display = 'none';
+    seccionRvaEspacios.style.display = 'none';
+  }
+
 });
+
